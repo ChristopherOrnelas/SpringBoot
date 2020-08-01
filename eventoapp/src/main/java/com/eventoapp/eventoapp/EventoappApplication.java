39ed0841e1921f863075d9322be6309cf7d78b13
Christopher S. Ornelas <68980556+ChristopherOrnelas@users.noreply.github.com>
2020-07-31 22:44:55 -0300

    Projeto funcionando so far

--- eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/EventoappApplication.java
@@ -2,12 +2,15 @@ package com.eventoapp.eventoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+//@ComponentScan({"com.eventoapp.controllers"})
+@EntityScan(basePackages={"com.eventoapp.models"})
 public class EventoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventoappApplication.class, args);
+    }
 
 }
