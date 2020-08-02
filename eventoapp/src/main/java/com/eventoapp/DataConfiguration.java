fd510a32a4762cb5b1b38e8908074a002e7d6534
Christopher S. Ornelas <68980556+ChristopherOrnelas@users.noreply.github.com>
2020-08-02 02:21:41 -0300

    Acesso ao banco MySQL

--- eventoapp/src/main/java/com/eventoapp/DataConfiguration.java
+++ eventoapp/src/main/java/com/eventoapp/DataConfiguration.java
@@ -1,6 +1,8 @@
 package com.eventoapp;
 
+
 import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
