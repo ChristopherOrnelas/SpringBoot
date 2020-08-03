1316a951a4dfc7389bf43f4550c5157c7a0a2356
Christopher S. Ornelas <68980556+ChristopherOrnelas@users.noreply.github.com>
2020-08-03 04:59:26 -0300

    Revert "Revert "Criada a aba detalhesEventos automatico, porem n retorna nada""
    This reverts commit cab796862fc5451f5a8198e35e527c865f6dd1d6.

--- eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
+++ eventoapp/src/main/java/com/eventoapp/controllers/EventoController.java
@@ -1,14 +1,15 @@
 package com.eventoapp.controllers;
 
+import com.eventoapp.models.Evento;
+import com.eventosapp.repository.EventoRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.eventoapp.models.Evento;
-import com.eventosapp.repository.EventoRepository;
 
 @EnableJpaRepositories(basePackages = "com.eventosapp.repository")
 @Controller
@@ -33,6 +34,14 @@ public class EventoController {
 		ModelAndView mv = new ModelAndView("index.html");
 		Iterable<Evento> eventos = er.findAll();
 		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo){
+		Evento evento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		mv.addObject("evento" + evento);
 		return mv;
 	}
 }
