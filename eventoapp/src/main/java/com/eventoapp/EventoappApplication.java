4ec1f23b87a7b0a8394f22de675b764ab07e31fb
Christopher S. Ornelas <68980556+ChristopherOrnelas@users.noreply.github.com>
2020-08-02 22:43:15 -0300

    teste

--- eventoapp/src/main/java/com/eventoapp/EventoappApplication.java
+++ eventoapp/src/main/java/com/eventoapp/EventoappApplication.java
@@ -3,7 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-
+//teste
 @SpringBootApplication
 @ComponentScan({"com.eventoapp.controllers"})
 @EntityScan(basePackages={"com.eventoapp.models"})
